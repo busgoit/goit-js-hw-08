b71f985c3245a272167a59896e7c4c26051872f6
busgoit <bus.go.it@gmail.com>
2022-08-07 22:55:39 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryElTemplate = ({ preview, original, description }) => {
+  return `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" data-caption="${description}" alt="${description}"/>
+    </a>`;
+};
+
+const galleryMarkup = galeryItemsArr => {
+  return galeryItemsArr.map(galleryElTemplate).join('');
+};
+
+const renderGallery = galleryArr => {
+  gallery.innerHTML = galleryMarkup(galleryArr);
+};
+
+renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  scrollZoom: false,
+  showCaptions: true,
+  captionType: 'data',
+  captionsData: 'caption',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
